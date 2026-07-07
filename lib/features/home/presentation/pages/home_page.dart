ad6eb1191f491a465904de3351c7ecca33a2161f
KarthikeyaJS <karthikeyajs2004@gmail.com>
2026-07-07 22:40:10 +0530

    fixed import issues with dashboard

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,17 +1,14 @@
+import 'package:campus_hub/features/auth/domain/usecases/logout_usecase.dart';
+import 'package:campus_hub/features/auth/presentation/cubit/auth_state_cubit/auth_state.dart';
+import 'package:campus_hub/features/auth/presentation/cubit/auth_state_cubit/auth_state_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../../../core/theme/app_text_styles.dart';
 import '../../../../di/injection_container.dart';
 import '../../../auth/domain/entities/user_role.dart';
-import '../../../auth/domain/usecases/logout_usecase.dart';
-import '../../../auth/presentation/cubit/auth_state_cubit/auth_state.dart';
-import '../../../auth/presentation/cubit/auth_state_cubit/auth_state_cubit.dart';
 import 'package:go_router/go_router.dart';
 
-/// Temporary landing page shown after login, regardless of role.
-/// Will be replaced by role-specific dashboards in later modules
-/// (Student Dashboard, Department Dashboard, Coordinator Dashboard).
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
